c2b8e9fe67ea3f966387f8ca757359f89a1d8fc8
Tyler <tcroberts@live.ca>
2019-12-18 19:12:54 -0800

    Try specifying coveralls version

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
         "tests": ["pytest==5.2.1",
                   "pytest-cov==2.8.1",
                   "coverage-badge",
-                  "coveralls"]
+                  "coveralls==1.9.2"]
     },
 )
